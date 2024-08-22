9c27a42252bfaa24cac4cfd6e79548b14df700b9
Ikrombek14 <ikrombekmurodov7@gmail.com>
2024-08-22 22:31:38 +0500

    fulle

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cuser.middleware.CuserMiddleware',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
